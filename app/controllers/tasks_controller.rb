079f60188fc405259b2132abfc7baabe902a882b
Tak.gldy <skypole6@gmail.com>
2020-11-14 10:30:54 +0000

    tasklist login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:create ,:edit , :update , :destroy]
+  before_action :correct_user, only: [:show ,:edit , :update , :destroy]
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -27,11 +26,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-       @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -43,7 +40,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
